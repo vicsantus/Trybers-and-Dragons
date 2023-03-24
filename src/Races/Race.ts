db04aeedcd499ed95442d37dfc89e5d6123a43b9
victrosantos <victor.santos.fk@hotmail.com>
2023-03-24 16:47:49 -0300

    Tirado coments

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -3,21 +3,11 @@ import { IRace } from '../interfaces/IRace';
 export default abstract class Race implements IRace {
   readonly name: string;
   readonly dexterity: number;
-  // protected static lastId = 0;
-  // protected _id: number;
 
   constructor(nm: string, dex: number) {
     this.name = nm;
     this.dexterity = dex;
-    // this._id = Race.newId();
   }
-
-  // private static newId() {
-  //   this.lastId += 1;
-  //   return this.lastId;
-  // }
-
-  // get id(): number { return this._id; }
 
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
